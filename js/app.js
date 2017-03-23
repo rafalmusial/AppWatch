f0dd8351730df1a1ad99e82a41559fc1594a43ca
rafalmusial <musial.rafal@gmail.com>
2017-03-23 12:16:54 +0100

    Wgrano preloader,ale strona dziala szybko, wiec nie jest potrzebny

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
 var toggleMenu = document.querySelector('.toggle');
 var listMenu = document.querySelector('nav');
 
+// Rozwijane menu w mobile
 toggleMenu.addEventListener("click", function(){
-
   listMenu.classList.toggle("show");
-
 })
 });
