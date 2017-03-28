0df1a7dc9f360a0710c0be2dafe019ef524451b3
rafalmusial <musial.rafal@gmail.com>
2017-03-28 18:09:06 +0200

    Edited app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-
 var toggleMenu = document.querySelector('.toggle');
 var listMenu = document.querySelector('nav');
-
 // Rozwijane menu w mobile
 toggleMenu.addEventListener("click", function(){
   listMenu.classList.toggle("show");
